52e2e1e0ce97ccf1e14b8b3c30d96a681be90826
Nathan Woo <natwoo97@gmail.com>
2017-03-07 15:40:17 -0800

    Complete ui outline

--- ui.R
+++ ui.R
@@ -10,17 +10,37 @@ ui <- fluidPage(
   sidebarLayout(
     # Sidebar
     sidebarPanel(
+      radioButtons("grades.button", "Grade of Interest:",
+                   c("6th Grade" = "sixth", 
+                     "7th Grade" = "seventh", 
+                     "8th Grade" = "eighth", 
+                     "10th Grade" = "tenth")),
       
+      br(),
+      
+      checkboxInput("subject.check", "Test Subject:",
+                    c("Reading" = "reading",
+                      "Math" = "math",
+                      "Writing" = "writing",
+                      "Science" = "science")),
+      
+      br(),
+      
+      sliderInput("years.slider", "Years:",
+                  min = 2010,
+                  max = 2013,
+                  value = 2013,
+                  step = 1)
     ),
     
     # Main panel
     mainPanel(
       # Create tabs
       tabsetPanel(type = "tabs",
-                  tabPanel(),
-                  tabPanel(),
-                  tabPanel(),
-                  tabPanel()
+                  tabPanel("Map"),
+                  tabPanel("Revenue vs. Test Score"),
+                  tabPanel("Data Table"),
+                  tabPanel("Aggregated Test Data")
       )
     )
   )
