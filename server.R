04f84944c7a1636e1ec23499b36ef2d2bc2c46e3
Nathan Woo <natwoo97@gmail.com>
2017-03-07 16:40:46 -0800

    Edit and aggregate data files

--- server.R
+++ server.R
@@ -3,9 +3,7 @@ library(ggplot2)
 library(dplyr)
 
 revenue.data <- read.csv("data/washington_school_district_level_revenue.csv", stringsAsFactors = FALSE)
-MSP.HSPE.data <- read.csv("data/2013_MSP_HSPE_Scores_by_District.csv", stringsAsFactors = FALSE)
-EOC.data <- read.csv("data/2013_EOC_Scores_by_District.csv", stringsAsFactors = FALSE)
-WAAS.data <- read.csv("data/2013_WAAS_Scores_by_District.csv", stringsAsFactors = FALSE)
+msp.hspe.data <- read.csv("data/2010_to_2013_MSP_HSPE_Scores_by_District.csv", stringsAsFactors = FALSE)
 
 server <- function(input, output) {
   
